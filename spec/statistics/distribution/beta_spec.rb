407936b4231280095896323e6140cad53f452cc7
esteban zapata <ezapata@altavistaed.com>
2017-08-17 12:28:44 -0500

    distribution: beta: Add Cumulative distribution function.
    This is a big change. It is composed by two changes:
      1. Implementation of the generalized incomplete beta function.
      2. The introduction of the cumulative distribution function, which
         is the same as the generalized incomplete beta function for the
         beta distribution.
    
    Woot!

--- spec/statistics/distribution/beta_spec.rb
+++ spec/statistics/distribution/beta_spec.rb
@@ -17,6 +17,39 @@ describe Statistics::Distribution::Beta do
     end
   end
 
+  describe '.incomplete_beta_function' do
+    it 'calculates the expected values for the incomplete beta function' do
+      # The last 2 values:
+      # For 9 is 0.9999979537560903519733 which is rounding to 1.0
+      # For 10 is 1.0
+      results = [0.19, 0.1808, 0.2557, 0.4059, 0.6230, 0.8418, 0.9685, 0.9985, 1.0, 1.0]
+
+      (1..10).each_with_index do |number, index|
+        expect(described_class.incomplete_beta_function(number/10.0, number, number + 1).round(4))
+          .to eq results[index]
+      end
+    end
+  end
+
+  describe '#cumulative_function' do
+    it 'returns one for values greater than one' do
+      expect(described_class.new(rand(10), rand(10)).cumulative_function(rand(2..10))).to eq 1
+    end
+
+    it 'is not defined for values less than zero' do
+      expect(described_class.new(rand(10), rand(10)).cumulative_function(rand(-5...0))).to be_nil
+    end
+
+    it 'returns the expected values for the beta distribution' do
+      result = [0.0523, 0.1808, 0.3483, 0.5248, 0.6875]
+      beta_distribution = described_class.new(2, 3) # alpha 2, beta 3
+
+      [0.1, 0.2, 0.3, 0.4, 0.5].each_with_index do |number, index|
+        expect(beta_distribution.cumulative_function(number).round(4)).to eq result[index]
+      end
+    end
+  end
+
   describe '#density_function' do
     it 'returns zero when the value is not defined in the [0,1] interval' do
       expect(described_class.new(1,1).density_function(2)).to eq 0
