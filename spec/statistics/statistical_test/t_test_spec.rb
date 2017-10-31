488a25791f938ef02c3b43c1c523ecba2d043444
Esteban Zapata Rojas <estebanz01@outlook.com>
2017-10-31 10:07:29 -0500

    statistical-test: Paired T-tests. (#14)

--- spec/statistics/statistical_test/t_test_spec.rb
+++ spec/statistics/statistical_test/t_test_spec.rb
@@ -57,4 +57,78 @@ describe Statistics::StatisticalTest::TTest do
       end
     end
   end
+
+  describe '.paired_test' do
+    # example ONE
+    # explained here: https://onlinecourses.science.psu.edu/stat500/node/51
+    # Trace metals in drinking water affect the flavor and an unusually high concentration can pose a health hazard.
+    # Ten pairs of data were taken measuring zinc concentration in bottom water and surface.
+    # Does the data suggest that the true average concentration in the bottom water exceeds that of surface water?
+    it 'performs a paired t-test following example ONE' do
+      group_one = [0.430, 0.266, 0.567, 0.531, 0.707, 0.716, 0.651, 0.589, 0.469, 0.723]
+      group_two = [0.415, 0.238, 0.390, 0.410, 0.605, 0.609, 0.632, 0.523, 0.411, 0.612]
+      alpha = 0.05
+
+      result = described_class.paired_test(alpha, :one_tail, group_one, group_two)
+
+      expect(result[:null]).to be false
+      expect(result[:alternative]).to be true
+    end
+
+    # example TWO
+    # explained here: https://www.statsdirect.com/help/parametric_methods/paired_t.htm
+    # Comparison of peak expiratory flow rate (PEFR) before and after a walk on a cold winter's day for a random sample of 9 asthmatics.
+    it 'performs a paired t-test following example TWO - one tail' do
+      before = [312, 242, 340, 388, 296, 254, 391, 402, 290]
+      after = [300, 201, 232, 312, 220, 256, 328, 330, 231]
+      alpha = 0.05
+
+      result = described_class.paired_test(alpha, :one_tail, before, after)
+
+      # https://github.com/estebanz01/ruby-statistics/issues/13
+      # In the example, the p-value for one-side is 0.0006 but we have 0.0004
+      # Apparently, it's how ruby handles and calculates the float points
+      # expect(result[:p_value]).to eq 0.0006
+      expect(result[:null]).to be false
+      expect(result[:alternative]).to be true
+    end
+
+    it 'performs a paired t-test following example TWO - two tail' do
+      before = [312, 242, 340, 388, 296, 254, 391, 402, 290]
+      after = [300, 201, 232, 312, 220, 256, 328, 330, 231]
+      alpha = 0.05
+
+      result = described_class.paired_test(alpha, :two_tail, before, after)
+
+      # https://github.com/estebanz01/ruby-statistics/issues/13
+      # In the example, the p-value for one-side is 0.0012 but we have 0.0008
+      # Apparently, it's how ruby handles and calculates the float points
+      # expect(result[:p_value].round(4)).to eq 0.0012
+      expect(result[:null]).to be false
+      expect(result[:alternative]).to be true
+    end
+
+    # Example THREE
+    # explained here: http://www.unm.edu/~marcusj/Paired2Sample.pdf
+    # we would hypothesize that the width of survey transects between individuals effects the
+    # density estimate of artifacts per unit area, however, we wish to test this hypothesis formally.
+    it 'performs a paired t-test following example THREE' do
+      five_mts = [9.998, 140.228, 40.669, 40.030, 86.292, 76.255, 16.689, 115.963, 161.497, 29.529,
+                  37.765, 16.919, 22.415, 4.496, 22.272, 73.061, 57.477, 64.188, 40.958, 10.224,
+                  91.245, 38.836, 160.985, 1.452, 209.540]
+      ten_mts = [15.384, 54.803, 38.913, 10.875, 10.733, 36.444, 34.774, 49.252, 51.759, 0.643,
+                 0.908, 114.969, 42.673, 43.370, 27.073, 19.343, 27.489, 2.808, 2.994, 95.575,
+                 53.564, 32.265, 42.102, 3.544, 11.333]
+      alpha = 0.05
+
+      result = described_class.paired_test(alpha, :two_tail, five_mts, ten_mts)
+
+      # https://github.com/estebanz01/ruby-statistics/issues/13
+      # In the example, the p-value for one-side is 0.0026 but we have 0.0024
+      # Apparently, it's how ruby handles and calculates the float points
+      # expect(result[:p_value].round(3)).to eq 0.026
+      expect(result[:null]).to be false
+      expect(result[:alternative]).to be true
+    end
+  end
 end
