869344849f34219f7fc3ac6c7571ef97db5db745
Esteban Zapata Rojas <estebanz01@outlook.com>
2018-01-16 00:00:55 -0500

    Generate gaussian random numbers (#16)
    * math: simpson rule: Ensure that x is integer for interval split.
    
    * distribution: normal: Generate random gaussian samples.
    
    * distribution: Weibull: Random sample that follows Weibull distribution.
    
    * distribution: Student's T: Generate random samples for T distribution.
    
    NOTE: The random sampling process is generation random values that
    are similar to an uniform random sample. Not sure why.
    
    * distribution: normal; weibull: Compare random samples against uniform.
    
    * distribution: t-student: Add warning to TStudent#random method.

--- lib/statistics/distribution/normal.rb
+++ lib/statistics/distribution/normal.rb
@@ -25,6 +25,46 @@ module Statistics
 
         (left_up/(left_down) * right)
       end
+
+      ## Marsaglia polar method implementation for random gaussian (normal) number generation.
+      # References:
+      # https://en.wikipedia.org/wiki/Marsaglia_polar_method
+      # https://math.stackexchange.com/questions/69245/transform-uniform-distribution-to-normal-distribution-using-lindeberg-l%C3%A9vy-clt
+      # https://www.projectrhea.org/rhea/index.php/The_principles_for_how_to_generate_random_samples_from_a_Gaussian_distribution
+
+      def random(elements: 1, seed: Random.new_seed)
+        results = []
+
+        # Setup seed
+        srand(seed)
+
+        # Number of random numbers to be generated.
+        elements.times do
+          x, y, r = 0.0, 0.0, 0.0
+
+          # Find an (x, y) point in the x^2 + y^2 < 1 circumference.
+          loop do
+            x = 2.0 * rand - 1.0
+            y = 2.0 * rand - 1.0
+
+            r = (x ** 2) + (y ** 2)
+
+            break unless r >= 1.0 || r == 0
+          end
+
+          # Project the random point to the required random distance
+          r = Math.sqrt(-2.0 * Math.log(r) / r)
+
+          # Transform the random distance to a gaussian value and append it to the results array
+          results << mean + x * r * standard_deviation
+        end
+
+        if elements == 1
+          results.first
+        else
+          results
+        end
+      end
     end
 
     class StandardNormal < Normal
