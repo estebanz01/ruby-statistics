d63153e09692f3cdd7fa4a3eb620a8ec7e3fcc26
Oliver Czulo <oliver.czulo@uni-leipzig.de>
2023-11-19 10:05:14 +0100

    Added functions to the chi_square test library to test contingency tables for independence of observations. Tested for plausibility for 2x2 and 3x3 tables

--- lib/statistics/statistical_test/chi_squared_test.rb
+++ lib/statistics/statistical_test/chi_squared_test.rb
@@ -37,6 +37,59 @@ module Statistics
           alternative: p_value <= alpha,
           confidence_level: 1 - alpha }
       end
+
+      # The following three functions serve to calculate a test of independence for contingency
+      # tables of the type
+      #
+      #      A   B
+      # X   20  18
+      # Y    7  35
+      #
+      # They have been tested using 2x2 and 3x3 tables.
+      #
+      def self.test_of_independence(alpha, observed)
+        expected = calculate_expected(observed)
+        df = (observed.size - 1) * (observed.transpose.size - 1)
+        chi_score = calculate_chi_score(observed, expected)
+        probability = 1.0 - Statistics::Distribution::ChiSquared.new(df).cumulative_function(chi_score)
+        p_value = 1.0 - probability
+    
+        {
+          chi_score: chi_score,
+          df: df,
+          probability: probability,
+          p_value: p_value,
+          alpha: alpha,
+          #null: alpha < p_value,
+          #alternative: p_value <= alpha,
+          confidence_level: 1 - alpha,
+          expected: expected
+        }
+      end
+      
+      # For a contingency table of observed values, calculate the expected values
+      def self.calculate_expected(observed)
+        row_sums = observed.map { |row| row.sum.to_f }
+        col_sums = observed.transpose.map { |col| col.sum.to_f }
+        total_sum = row_sums.sum
+    
+        expected = observed.map.with_index { |row, i|
+          row.map.with_index { |obs, j|
+            (row_sums[i] * col_sums[j]) / total_sum
+          }
+        }
+        expected
+      end
+    
+      def self.calculate_chi_score(observed, expected)
+        sum = 0.0
+        observed.size.times { |i|
+          observed[i].size.times { |j|
+            sum += ((observed[i])[j] - (expected[i])[j])**2 / (expected[i])[j]
+          }
+        }
+        sum
+      end
     end
   end
 end
