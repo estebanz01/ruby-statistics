057a74e1620fd576657a049e3d49bfd9e75b65f9
Oliver Czulo <oliver.czulo@uni-leipzig.de>
2024-01-10 21:22:49 +0100

    contingency tables implemented as Matrix, +test

--- lib/statistics/statistical_test/chi_squared_test.rb
+++ lib/statistics/statistical_test/chi_squared_test.rb
@@ -1,6 +1,9 @@
 module Statistics
   module StatisticalTest
     class ChiSquaredTest
+
+      require 'matrix'
+
       def self.chi_statistic(expected, observed)
         # If the expected is a number, we asumme that all expected observations
         # has the same probability to occur, hence we expect to see the same number
@@ -39,7 +42,7 @@ module Statistics
       end
 
       # The following three functions serve to calculate a test of independence for contingency
-      # tables of the type
+      # tables (short: ct) of the type
       #
       #      A   B
       # X   20  18
@@ -47,10 +50,10 @@ module Statistics
       #
       # They have been tested using 2x2 and 3x3 tables.
       #
-      def self.test_of_independence(alpha, observed)
-        expected = calculate_expected(observed)
-        df = (observed.size - 1) * (observed.transpose.size - 1)
-        chi_score = calculate_chi_score(observed, expected)
+      def self.test_of_independence_matr(alpha, observed_matr)
+        expected_matr = calculate_expected_matr(observed_matr)
+        df = (observed_matr.row_size - 1) * (observed_matr.column_size - 1)
+        chi_score = chi_statistic_matr(observed_matr, expected_matr)
         probability = 1.0 - Statistics::Distribution::ChiSquared.new(df).cumulative_function(chi_score)
         p_value = 1.0 - probability
     
@@ -60,34 +63,36 @@ module Statistics
           probability: probability,
           p_value: p_value,
           alpha: alpha,
-          #null: alpha < p_value,
-          #alternative: p_value <= alpha,
+          null: alpha < p_value,
+          alternative: p_value <= alpha,
           confidence_level: 1 - alpha,
-          expected: expected
+          expected: expected_matr
         }
       end
       
       # For a contingency table of observed values, calculate the expected values
-      def self.calculate_expected(observed)
-        row_sums = observed.map { |row| row.sum.to_f }
-        col_sums = observed.transpose.map { |col| col.sum.to_f }
+      def self.calculate_expected_matr(observed_matr)
+        row_sums = observed_matr.row_vectors.map { |row| row.to_a.sum.to_r }
+        col_sums = observed_matr.column_vectors.map { |col| col.to_a.sum.to_r }
         total_sum = row_sums.sum
-    
-        expected = observed.map.with_index { |row, i|
-          row.map.with_index { |obs, j|
-            (row_sums[i] * col_sums[j]) / total_sum
-          }
-        }
-        expected
+        
+        # create a mutable array from the Matrix of observed values
+        # so we have a 'template' for our Matrx of expected values
+        expected = observed_matr.to_a
+        # calculate the expected values
+        observed_matr.each_with_index do |i, row, col|
+          expected[row][col] = (row_sums[row] * col_sums[col]) / total_sum
+        end
+        Matrix.rows(expected)
       end
     
-      def self.calculate_chi_score(observed, expected)
+      private
+
+      def self.chi_statistic_matr(observed_matr, expected_matr)
         sum = 0.0
-        observed.size.times { |i|
-          observed[i].size.times { |j|
-            sum += ((observed[i])[j] - (expected[i])[j])**2 / (expected[i])[j]
-          }
-        }
+        observed_matr.each_with_index do |i, row, col|
+            sum += (observed_matr[row, col] - expected_matr[row, col])**2 / expected_matr[row, col]
+        end
         sum
       end
     end
